a90ddf73b9ac1c2a9ead30731dc8831d53d0c4ea
phaninho <phaninho@live.fr>
2018-09-24 23:25:33 +0200

    add exo to level08

--- level03/Ressources/script.sh
+++ level03/Ressources/script.sh
@@ -2,7 +2,7 @@
 #   >exploit me
 # en faisant un nm level03 on vois la presence d'un appel a system
 # en faisant un strings level03 on trouve /usr/bin/env echo Exploit me
-# on peu deduire que le code fais un appel a echo par la fonction system pour afficher exploit ne
+# on peu deduire que le code fais un appel a echo par la fonction system pour afficher exploit me
 # system est vulnerable car il parcours la variable $PATH et a la premiere occurence du programme trouve il l'execute.
 # l'astuce consiste a mettre notre programme qui s'appel aussi echo en debut de liste mais celui ci sera en fait le programme getflag
 
